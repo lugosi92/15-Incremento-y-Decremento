29382f3af997a3ceab5ac8e4fc8675d2b05c9bc9
khaled___beno <khaled___beno@localhost>
2023-12-26 19:27:25 +0100

    Incremento y decremento

---
+++ 15_Incrementos y decrementos/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numero = 5;
+		
+		numero --;
+		numero++;
+		numero ++;
+
+		
+		System.out.println("Numero es =" + numero);
+	}
+
+}
